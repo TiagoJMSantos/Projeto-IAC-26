baed0a5114d13d2eee0dfc61fb090f8bc6b31f86
TiagoJMSantos <b0tgh0stlyg@gmail.com>
2026-04-28 13:48:48 +0100

    feat: final version of select file

--- select.s
+++ select.s
@@ -27,6 +27,22 @@ exit:
 # ===========================================================================
 select:
   # TODO: Implement the select function here
+     li t3, 1 # starts min array size
+     li t2, 0 # starts final value
+     blt a2, t3, select_invalid # error if a2<t3
+     bgt a3, a2, select_invalid_2 # error if a3>a2
+     slli t0, a3, 2 # index in bytes, word is 4 bytes
+     add t0, a1, t0 # shifts array a1 to index t0
+     lw t2, 0(t0) # t0 is array[i]
+     mv a1, t2 # moves value of t2 to the selected element
+  
+select_invalid:
+    li a0, 50    # returns code 50 -> argument invalid
+    ret   
+    
+select_invalid_2:
+    li a0, 100    # returns code 100 -> index out of limit
+    ret
 
 select_end:
-  jr ra               # return to the caller
+     jr ra               # return to the caller
