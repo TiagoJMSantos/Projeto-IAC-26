4fd5610e5d97fee52033460aff0e767d14020a55
diogocerdeirarodrigues-design <diogo.cerdeira.rodrigues@tecnico.ulisboa.pt>
2026-05-08 07:45:54 +0000

    Corrigido transição do loop para o final através de "j select_end"

--- select.s
+++ select.s
@@ -1,8 +1,7 @@
-# You can change these values to test your solution.
 .data
-ARRAY: .word -6 -1 6 1
-SIZE:  .word 4
-INDEX: .word 2
+ARRAY: .word 42
+SIZE:  .word 1
+INDEX: .word 1
 
 .text
 main:
@@ -26,23 +25,30 @@ exit:
 #   a1 = value of the selected element
 # ===========================================================================
 select:
-  # TODO: Implement the select function here
      li t3, 1 # starts min array size
      li t2, 0 # starts final value
      blt a2, t3, select_invalid # error if a2<t3
-     bgt a3, a2, select_invalid_2 # error if a3>a2
+     blt a3, x0, select_invalid_2 # error if a3<0
+     bge a3, a2, select_invalid_2 # error if a3>=a2
      slli t0, a3, 2 # index in bytes, word is 4 bytes
      add t0, a1, t0 # shifts array a1 to index t0
-     lw t2, 0(t0) # t0 is array[i]
+     lw t2, 0(t0) # t2 is array[i]
+     li a0, 0 # returns code 0 -> success
      mv a1, t2 # moves value of t2 to the selected element
+     j select_end
   
 select_invalid:
     li a0, 50    # returns code 50 -> argument invalid
-    ret   
+    li a1, 0
+    j select_end
     
 select_invalid_2:
     li a0, 100    # returns code 100 -> index out of limit
-    ret
+    li a1, 0
+    j select_end
+
+select_end:
+     jr ra               # return to the caller
 
 select_end:
      jr ra               # return to the caller
